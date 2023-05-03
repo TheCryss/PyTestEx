a64ea4756a05caec9fc94414ab9a0184bd814725
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-05-03 00:15:30 -0500

    first commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+import math
+
+def add(x,y):
+    return x+y
+
+def substraction(x,y):
+    return x -y
+
+def prod(x,y):
+    return x*y
+
+def power(x,y):
+    return math.pow(x,y)
---
+++ test_calc.py
@@ -0,0 +1,58 @@
+import pytest
+from calc import *
+
+
+#Prueba unitaria
+@pytest.mark.skip(reason="Porque si")
+def test_add():
+    assert add(2,2) == 4 
+
+#Prueba en serie
+
+@pytest.mark.parametrize(
+    "input_a,input_b,expected",
+    [
+        (2,1,3),
+        (3,-2,1),
+        (add(3,1),4,9),
+        (10,2,12)
+        ]
+)
+#con el comando $ pytest -v podemos ver el estado de ejecucion de las pruebas
+def test_add_multi(input_a,input_b,expected):
+    assert add(input_a,input_b) == expected
+
+
+#Si lo ejecutamos con $ pytest -v -k "substraction" filtramos todos los test cuyo nombre contenga substraction
+#Tambien podemos aplicar or y and $ pytest -v -k "substraction or multi" || $ pytest -v -k "add and multi"
+def test_substraction():
+    assert substraction(2,2) == 0  
+
+#Tambien se pueden filtrar mediante decoradores
+#$ pytest -v -m number
+
+@pytest.mark.number
+@pytest.mark.parametrize(
+        "input_a,input_b,expected",
+        [
+            (2,0,1),
+            (2,1,2),
+            (2,2,4),
+            (2,3,8)
+        ]
+)
+def test_power_multi(input_a,input_b,expected):
+    assert power(input_a,input_b) == expected
+
+@pytest.mark.number
+def test_substraction_mark():
+    assert substraction(2,2) == 0  
+
+#Con el parametro -x cuando ocurra un error los test faltantes seran detenidos
+#$ pytest -v -x
+
+#Con el parametro --tb=no no se muestra el stack trace del error
+#$ pytest -v --tb=no
+
+#Mediante el parametro --maxfail=2 paramos la ejecucion del los test cuando 2 fallan
+#$ pytest -v --maxfail=2
